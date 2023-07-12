8b37fa57959da13acb6aaeab74104b73cec01a17
aiapo <agitschl@umflint.edu>
2023-07-12 16:00:13 -0400

    fix to com.todo & try to add jdbc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,12 @@
-module com.example.demo {
+module com.todo {
     requires javafx.controls;
     requires javafx.fxml;
 
             requires com.dlsc.formsfx;
                 requires org.kordamp.ikonli.javafx;
             requires org.kordamp.bootstrapfx.core;
-            
+    requires java.sql;
+
     opens com.todo to javafx.fxml;
     exports com.todo;
 }
