32607142098f212dfef48da8af12eb26094f3d00
aiapo <agitschl@umflint.edu>
2023-07-14 16:58:12 -0400

    Make Database use new QueryBuilder

--- src/main/java/com/todo/Database.java
+++ src/main/java/com/todo/Database.java
@@ -2,83 +2,87 @@ package com.todo;
 
 import java.sql.*;
 public class Database {
-    // ABILITY TO USE DIFFERENT DATABASE FILES
-    static String databaseFile;
-    public Database(String s) {
-        databaseFile = s;
+
+    // Create a database connection
+    protected Connection connection;
+
+    // Import the Query class (this class will format the queries)
+    protected QueryBuilder query;
+
+    // Constructor to create connection to specified database
+    public Database(String databaseFile) throws SQLException{
+        connection = DriverManager.getConnection("jdbc:sqlite:"+databaseFile);
     }
 
-    // CREATE SQLITE DATABASE
-    public static boolean createDB(){
-        Connection c;
-        Statement stmt;
-
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:"+databaseFile);
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            return false;
+    // Execute the queries
+    // This takes the prepared query (ex. DELETE FROM ? WHERE ? = ?)
+    // And the params and creates a prepared statement for efficiency
+    // Then executes...
+    // Special cases: CREATE and SELECT
+    private int execute(String query, Object[] params) throws SQLException{
+        PreparedStatement ps = connection.prepareStatement(query);
+        if (params != null){
+            int count = params.length;
+            for (int i = 0; i<count; i++) {
+                ps.setObject(i, params[i].toString());
+            }
         }
-        return true;
+        return ps.executeUpdate();
     }
 
-    // CREATE TABLE IN SQLITE DATABASE
-    public static boolean createDBTables(String sql){
-        Connection c;
-        Statement stmt;
+    // Creates a new table based on an object of attributes
+    public int create(String table, Object[] params) throws SQLException{
+        query = new QueryBuilder();
 
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:"+databaseFile);
+        query.create(table).attributes(params);
 
-            stmt = c.createStatement();
+        return execute(query.stringifyQuery(), null);
+    }
 
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            return false;
+    // Inserts into a table an object of values
+    public int insert(String table, Object[] params) throws SQLException{
+        query = new QueryBuilder();
+
+        query.insert(table).values(params);
+
+        return execute(query.stringifyQuery(), params);
+    }
+
+
+    // Updates a tuple in a table based on where condition
+    public int update(String table, String[] tuple, String condition, Object[] params) throws SQLException{
+        query = new QueryBuilder();
+
+        query.update(table).set(tuple).where(condition);
+
+        return execute(query.stringifyQuery(), params);
+    }
+
+    public ResultSet select(String table, Object[] columns, Object[] params) throws SQLException{
+        return this.select(table, columns, "", params);
+    }
+
+    public ResultSet select(String table, Object[] columns, String condition, Object[] params) throws SQLException{
+        query = new QueryBuilder();
+        query.select(columns).from(table).where(condition);
+
+        PreparedStatement ps = connection.prepareStatement(query.stringifyQuery());
+        if(params != null){
+            int index = 1;
+            for(Object param : params){
+                ps.setObject(index, param);
+                index++;
+            }
         }
-        return true;
-    }
 
-    // INSERT BASED ON PASSED QUERY, USE WRAPPER
-    public static boolean insertDB(String sql){
-        Connection c;
-        Statement stmt;
-
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:"+databaseFile);
-            c.setAutoCommit(false);
-
-            stmt = c.createStatement();
-            stmt.executeUpdate(sql);
-
-            stmt.close();
-            c.commit();
-            c.close();
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            return false;
-        }
-        return true;
-    }
-
-    // SELECT BASED ON PASSED QUERY, USE WRAPPER
-    public static ResultSet selectDB(String sql) throws SQLException, ClassNotFoundException {
-        Connection c;
-        Statement stmt;
-
-        Class.forName("org.sqlite.JDBC");
-        c = DriverManager.getConnection("jdbc:sqlite:test.db");
-        c.setAutoCommit(false);
-        System.out.println("Opened database successfully");
-
-        stmt = c.createStatement();
-        ResultSet rs = stmt.executeQuery(sql);
+        ResultSet rs = ps.executeQuery();
         return rs;
+    }
+
+    // Deletes from a table based on a where condition
+    public int delete(String table, String requirement, Object[] param) throws SQLException{
+        query = new QueryBuilder();
+        query.delete(table).where(requirement);
+        return execute(query.stringifyQuery(), param);
     }
 }
