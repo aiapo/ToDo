79fedbddd3f7e592df6f36e04e8c6527af1140fa
aiapo <agitschl@umflint.edu>
2023-07-14 16:58:30 -0400

    Update TaskDatabase to new Database

--- src/main/java/com/todo/TaskDatabase.java
+++ src/main/java/com/todo/TaskDatabase.java
@@ -1,31 +1,44 @@
 package com.todo;
 
 import java.io.File;
+import java.sql.SQLException;
 
 public class TaskDatabase {
+    // For this project we are using SQLite, just call our database file tasks.db
     static String databaseName = "tasks.db";
 
-    static Database toDoDB = new Database(databaseName);
-    public static boolean initTaskDB(){
-        String tableCreationSQL =
-                "CREATE TABLE TASKS " +
-                        "(ID             INTEGER     PRIMARY KEY    AUTOINCREMENT," +
-                        " NAME           TEXT    NOT NULL, " +
-                        " CREATION_DATE  TEXT    NOT NULL, " +
-                        " DUE_DATE       TEXT    NOT NULL," +
-                        " COMPLETION     INTEGER    NOT NULL)";
-        if (new File(databaseName).length() <= 0) {
-            if(!toDoDB.createDB())
-                return false;
-            if(!toDoDB.createDBTables(tableCreationSQL))
-                return false;
+    // Create the database/connect to it
+    static Database database;
+    static {
+        try {
+            database = new Database("tasks.db");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return true;
     }
 
-    public static boolean insertTaskDB(String insertName, String createDate, String dueDate, Integer completeStatus){
-        String insertTaskSQL = "INSERT INTO TASKS (NAME,CREATION_DATE,DUE_DATE,COMPLETION) " +
-                "VALUES ('"+insertName+"','"+createDate+"','"+dueDate+"',"+completeStatus+");";
-        return toDoDB.insertDB(insertTaskSQL);
+    // Initialize the database if it hasn't been already (create tables)
+    public static void initTaskDB(){
+        if (new File(databaseName).length() <= 0) {
+            try {
+                String newTable = "Tasks";
+                Object[] attributes = {
+                        "ID INTEGER PRIMARY KEY AUTOINCREMENT",
+                        "NAME   TEXT    NOT NULL",
+                        "DESCRIPTION    TEXT    NOT NULL",
+                        "CREATION_DATE  TEXT    NOT NULL",
+                        "DUE_DATE   TEXT    NOT NULL",
+                        "COMPLETION INTEGER NOT NULL"
+                };
+                int response = database.create(newTable, attributes);
+                if(response==0)
+                    System.out.println("Created table "+newTable);
+                else
+                    System.out.println("Table creation failed for "+newTable);
+            } catch (SQLException error) {
+                System.out.println("Error: "+error.getMessage());
+            }
+        }
     }
+
 }
