652595cafc8bbb653b696ca4098249e9427c1c40
aiapo <agitschl@umflint.edu>
2023-07-15 17:22:09 -0400

    Add testing of selections

--- src/main/java/com/todo/TaskManager.java
+++ src/main/java/com/todo/TaskManager.java
@@ -1,15 +1,18 @@
 package com.todo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class TaskManager {
-    protected static TaskDatabase tDB;
+    //public TaskDatabase tDB;
+    TaskDatabase tDB = new TaskDatabase();
 
-    public TaskManager(){
+    public TaskManager() throws SQLException {
         tDB.init();
     }
 
-    public static void add(String name, String description, String creation, String due, Integer complete){
+    public void add(String name, String description, String creation, String due, Integer complete){
         Object[] newTask = {0, name, description, creation, due, complete};
         if(tDB.insert("Tasks",newTask)){
             System.out.println("Task "+name+" created!");
@@ -17,6 +20,15 @@ public class TaskManager {
             System.out.println("Error creating "+name+"!");
         }
 
+    }
+
+    public void get() throws SQLException {
+        String[] attributes = {"name", "description"};
+        Object[] is = {"0"};
+        ResultSet rs = tDB.select("Tasks", attributes, "completion = ?", is);
+        while(rs.next()) {
+            System.out.println(rs.getString("name") + ": " + rs.getString("description"));
+        }
     }
 
 }
