4a5a14f9f49964679d3deb6613911c0949f0d301
aiapo <agitschl@umflint.edu>
2023-07-20 17:00:05 -0400

    Add new program icon

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -3,6 +3,7 @@ package com.todo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("ToDo");
+        stage.setTitle("ToDo Manager");
+        stage.getIcons().add(new Image("file:icon.png"));
         stage.setScene(scene);
         stage.show();
     }
